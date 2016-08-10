84dc9cc5969cd71fe28080b5419e1dcdd422b355
Seung-hae KIM <aka.seunghaekim@gmail.com>
2016-08-10 22:58:45 +0900

    django

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\github\aka.seunghaekim\seunghaekim.github.io\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
