3c5c633fb5e77be48b368c5b58a8852625d04638
Seung-hae KIM <aka.seunghaekim@gmail.com>
2016-11-28 09:25:34 +0900

    daily fix

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\github\aka.seunghaekim\seunghaekim.github.io\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
