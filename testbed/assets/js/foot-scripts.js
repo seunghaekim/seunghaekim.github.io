1634a2c1037ef5133f3afa10fe339323afba2584
Seung-hae KIM <aka.seunghaekim@gmail.com>
2016-11-29 16:16:14 +0900

    test dev

---
+++ testbed/assets/js/foot-scripts.js
@@ -0,0 +1,10 @@
+// var row_spanX4_height;
+// $(document).ready(function(){
+//     $( window ).resize(function(){
+//         row_spanX4_height = $(".row-spanX4").height();
+//         $(".slick-slide").height(row_spanX4_height).css({
+//             width: "auto"
+//         })
+//         console.log(row_spanX4_height);
+//     });
+// })
--- testbed/js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $(".gallery").justifiedGallery({
-        rowHeight: 120
-    });
-})
