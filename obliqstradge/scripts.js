4b75e018c45aad7d13eb73afb81eecefea4386c4
Seung-hae KIM <aka.seunghaekim@gmail.com>
2016-12-01 18:25:01 +0900

    update

--- obliqstradge/scripts.js
+++ obliqstradge/scripts.js
@@ -1,6 +1,24 @@
-var target = "https://raw.githubusercontent.com/seunghaekim/the-legendary-computer/test-bed/list-of-articles.json";
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var forepix = "https://raw.githubusercontent.com/seunghaekim/seunghaekim.github.io/master/obliqstradge/";
+
+var decks = [
+    forepix + "obliqstradge.ed1.json",
+    forepix + "obliqstradge.ed2.json",
+    forepix + "obliqstradge.ed3.json",
+    forepix + "obliqstradge.ed4.json",
+];
+
+function rand_deck (input) {
+    return input[rand(0, input.length)];
+}
+
+var deck = rand_deck(decks);
 
 $(document).ready(function(){
-    $.getJSON(target, function(result) {
+    $.getJSON(deck, function(result) {
+        console.log(result.deck);
     })
 })
