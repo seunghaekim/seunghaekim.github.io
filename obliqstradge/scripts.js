7b2af1527ff130ef07313c27fc176a488f1fbfe2
Seung-hae KIM <aka.seunghaekim@gmail.com>
2016-12-01 18:04:36 +0900

    update the obliqstradge

---
+++ obliqstradge/scripts.js
@@ -0,0 +1,6 @@
+var target = "https://raw.githubusercontent.com/seunghaekim/the-legendary-computer/test-bed/list-of-articles.json";
+
+$(document).ready(function(){
+    $.getJSON(target, function(result) {
+    })
+})
